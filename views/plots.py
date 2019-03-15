849510eef3f23ea9edf5c3bfe6be255eec6840eb
Anthony Laye <anthony_laye@hotmail.com>
2019-03-15 16:36:15 -0400

    UI changes

--- views/plots.py
+++ views/plots.py
@@ -37,6 +37,8 @@ class Plots(tk.Tk):
 
         matplotlib.rcParams['lines.linewidth'] = 3
 
+        self.redundancy_gps = False  # Boolean to either graph GPS or redundancy GPS
+
         """ Telemetry values """
         self.time_list = []
         self.temperature_list = []
@@ -48,24 +50,29 @@ class Plots(tk.Tk):
         self.latitude_list = []
         self.longitude_list = []
 
+        """ Redundancy GPS values """
+        self.redundancy_latitude_list = []
+        self.redundancy_longitude_list = []
+
         """ Build main frame"""
         self.state('zoomed')  # Make fullscreen by default
 
-        container = tk.Frame(self, bg='blue')
-        container.pack(side="top", fill="both", expand=True)
+        label = tk.Label(self, bg='black', fg='white', text="McGill Rocket Team Ground Station", font=("Courier", 15), width=400)
+        label.pack(pady=10, padx=10)
+
+        container = tk.Frame(self, bg='black')
+        container.pack()
         container.grid_rowconfigure(0, weight=1)
         container.grid_columnconfigure(0, weight=1)
 
-        self.frame = GraphPage(container)
-        self.page = GraphPage
-        self.frame.configure(background='black')
-        self.frame.grid(row=0, column=0, sticky="nsew")
-        self.show_frame()  # Display frame on window
+        canvas = FigureCanvasTkAgg(plots, self)
+        canvas.draw()
+        canvas.get_tk_widget().pack(side=tk.BOTTOM, fill=tk.BOTH, expand=True)
 
         """ Antenna calculations angle label """
         self.antennaAngle = tk.Label(self, text='ANTENNA ANGLE: 0 (xy), 0 (z)')
-        self.antennaAngle.config(font=("Courier", 25))
-        self.antennaAngle.pack(pady=10, padx=10)
+        self.antennaAngle.config(font=("Courier", 15))
+        self.antennaAngle.pack()
 
         """ Reset graph functions """
         def reset_telemetry_graphs():
@@ -82,13 +89,16 @@ class Plots(tk.Tk):
 
         """ Reset graph buttons """
         reset_telemetry_button = tk.Button(self, text="RESET TEL", command=reset_telemetry_graphs)
-        reset_telemetry_button.pack()
+        reset_telemetry_button.pack(side=tk.LEFT)
         reset_gps_button = tk.Button(self, text="RESET GPS", command=reset_gps_graphs)
-        reset_gps_button.pack()
+        reset_gps_button.pack(side=tk.LEFT)
 
-    def show_frame(self):
-        frame = self.frame
-        frame.tkraise()
+        def toggle_gps_graph():
+            self.redundancy_gps = not self.redundancy_gps  # Toggle GPS
+
+        """ Toggle GPS graph button """
+        toggle_gps_graph_button = tk.Button(self, text="GPS", command=toggle_gps_graph)
+        toggle_gps_graph_button.pack(side=tk.RIGHT)
 
     def plot_telemetry_data(self, telemetry_data):
         """ Append telemetry data to lists """
@@ -101,24 +111,24 @@ class Plots(tk.Tk):
         """ Plot data """
         self.temperature_graph.clear()
         self.temperature_graph.plot(self.time_list, self.temperature_list)  # Graph temperature
-        self.temperature_graph.set_ylabel('Temperature(Celcius)')
+        self.temperature_graph.set_ylabel('Temperature')
 
         self.altitude_graph.clear()
         self.altitude_graph.plot(self.time_list, self.altitude_list)  # Graph altitude
-        self.altitude_graph.set_ylabel('Altitude(m)')
+        self.altitude_graph.set_ylabel('Altitude')
 
         self.velocity_graph.clear()
         self.velocity_graph.plot(self.time_list, self.velocity_list)  # Graph velocity
-        self.velocity_graph.set_ylabel('Velocity(m/s)')
+        self.velocity_graph.set_ylabel('Velocity')
 
         self.acceleration_graph.clear()
         self.acceleration_graph.plot(self.time_list, self.acceleration_list)  # Graph acceleration
         self.acceleration_graph.set_xlabel('Time(s)')
-        self.acceleration_graph.set_ylabel('Acceleration(m/s^2)')
+        self.acceleration_graph.set_ylabel('Acceleration')
 
     def plot_gps_data(self, telemetry_data):
         """ Append GPS data to lists """
-        utm_coordinates = utm.from_latlon(float(telemetry_data[0]), float(telemetry_data[1])) # Convert to UTM coordinates
+        utm_coordinates = utm.from_latlon(float(telemetry_data[0]), float(telemetry_data[1]))  # Convert to UTM coordinates
         self.latitude_list.append(utm_coordinates[0])
         self.longitude_list.append(utm_coordinates[1])
 
@@ -131,16 +141,3 @@ class Plots(tk.Tk):
         global plots
         plots.canvas.draw()  # Update plots
         plots.canvas.flush_events()  # Flush the GUI events
-
-
-class GraphPage(tk.Frame):
-    def __init__(self, parent):
-        tk.Frame.__init__(self, parent)
-
-        """ Set up Graph Page"""
-        label = tk.Label(self, bg='red', text="McGill Rocket Team Ground Station", font=LARGE_FONT, width=400)
-        label.pack(pady=10, padx=10)
-
-        canvas = FigureCanvasTkAgg(plots, self)
-        canvas.draw()
-        canvas.get_tk_widget().pack(side=tk.BOTTOM, fill=tk.BOTH, expand=True)
